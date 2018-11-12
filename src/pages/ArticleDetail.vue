ead74cabd2086ec92fd0447b0db3db3f2afcc1e5
lbgod2222 <1097843969@qq.com>
2018-11-13 00:18:46 +0800

    :star: Adjust: fulfill article detail page

--- src/pages/ArticleDetail.vue
+++ src/pages/ArticleDetail.vue
@@ -1,9 +1,28 @@
 <template>
   <q-page padding>
     <!-- 头部内容 -->
-    <header>
-      {{article.title}}
+    <header class="row">
+      <div class="article-title text-secondary uppercase offset-3">
+        {{article.title}}
+      </div>
+      <div class="article-subtitle text-neutral offset-3">
+        LastModified: {{article.lastModified}}
+      </div>
     </header>
+    <div class="row content mt-2">
+      <aside class="col-3 row">
+        <div class="aside-wrapper">
+          <div class="tags justify-start cursor-pointer">
+            <span class="tag uppercase" v-for="(item, idx) in article.meta.tags" :key="idx">
+              {{item}}
+            </span>
+          </div>
+        </div>
+      </aside>
+      <article class="col-8 article-content">
+        {{article.content}}
+      </article>
+    </div>
   </q-page>
 </template>
 
@@ -21,15 +40,15 @@ export default {
     return {
       article: {
         'meta': {
-          'tags': [],
-          'votes': 0,
-          'cultivated': 0
+          'tags': ['true', 'false', 'do', 'you', 'want', 'me'],
+          'votes': 38,
+          'cultivated': 5
         },
         'lastModified': '2018-08-18T16:00:00.000Z',
         'comments': [],
         '_id': '5b787ff631e1a940d4732357',
-        'title': 'The seventh article',
-        'content': 'This should be a very long content that i have never seen, trust me there!',
+        'title': '即使没有更多情况我也要进行一次中文测试',
+        'content': 'This should be a very long content that i have never seen, trust me there! This should be a very long content that i have never seen, trust me there! This should be a very long content that i have never seen, trust me there! This should be a very long content that i have never seen, trust me there! This should be a very long content that i have never seen, trust me there! This should be a very long content that i have never seen, trust me there! ',
         '__v': 0
       }
     }
@@ -37,5 +56,16 @@ export default {
 }
 </script>
 
-<style>
+<style lang="stylus" scoped>
+@import '~variables'
+.tag
+  display inline-block
+  border 1px solid $info
+  color $info
+  padding 5px 8px
+  margin-right 5px
+  margin-bottom 5px
+  &:hover
+    border 1px solid $negative
+    color $negative
 </style>
